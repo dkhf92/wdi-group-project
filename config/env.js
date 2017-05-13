0b3dc02f6b4f2237826c66cc817a949582114c3d
dkhf92 <didrik@forsberg.to>
2017-05-13 14:34:18 +0100

    :fire:

--- config/env.js
+++ config/env.js
@@ -1,8 +1,5 @@
 module.exports = {
   env: process.env.NODE_ENV,
-  db: {
-    production: process.env.MONGOD_URI,
-    development: `mongodb://localhost/tasks-${this.env}`,
-    test: `mongodb://localhost/tasks-${this.env}`
-  }
+  db: 'mongodb://localhost/thisApp',
+  secret: process.env.SECRET || 'this is so secret OMFG'
 };
