7d63fcefbfe95a12e73f8c7940811e92dc36c784
mmorandini <emmemorandini@gmail.com>
2017-05-13 13:43:00 +0100

    created task schema

---
+++ models/task.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: true },
+  description: { type: String, trim: true, required: true },
+  location: { type: String, trim: true, required: true },
+  price: { type: Number, required: true}
+},{
+  timestamps: true
+});
+
+taskSchema
+  .path('price')
+  .get(convertToDecimal)
+  .set(convertFromDecimal);
+
+function convertToDecimal(value){
+  return(value/100).toFixed(2);
+}
+
+function convertFromDecimal(value){
+  return value * 100;
+}
+module.exports = mongoose.model('Task', taskSchema);
