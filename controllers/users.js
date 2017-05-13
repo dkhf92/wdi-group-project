d58014d3d8463ff0a7d5ca229dfac818eb3262bb
dkhf92 <didrik@forsberg.to>
2017-05-13 13:42:10 +0100

    created functions in users.js controller

--- controllers/users.js
+++ controllers/users.js
@@ -16,10 +16,26 @@ function usersShow(req, res) {
   });
 }
 
+function usersUpdate(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body.user, { new: true }, (err, user) => {
+    if (err) return res.status(500).json({ message: 'Something went wrong.' });
+    if (!user) return res.status(404).json({ message: 'User not found.' });
+    return res.status(200).json(user);
+  });
+}
+
+function usersDelete(req, res) {
+  User.findByIdAndRemove(req.params.id, (err, user) => {
+    if (err) return res.status(500).json({ message: 'Something went wrong.' });
+    if (!user) return res.status(404).json({ message: 'User not found.' });
+    return res.sendStatus(204);
+  });
+}
 
 
 module.exports = {
   index: usersIndex,
-  show: usersShow
-
+  show: usersShow,
+  update: usersUpdate,
+  delete: usersDelete
 };
