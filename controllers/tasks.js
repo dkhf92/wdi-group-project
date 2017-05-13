123da74a457f99281927d452cdd2547322bd5b00
mmorandini <emmemorandini@gmail.com>
2017-05-13 14:13:49 +0100

    added functions in task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,32 @@
 const Task = require('../models/task');
 
-function taskIndex(){
-  
+function taskIndex(req, res, next){
+  Task
+  .find()
+  .exec()
+  .then(tasks => {
+    return res.status(200).json(tasks);
+  })
+  .catch(next);
 }
+
+function taskShow(req, res, next){
+  Task
+  .findById(req.params.id)
+  .exec()
+  .then(task => {
+    if(!task){
+      const error = new Error('No task has been found');
+      error.status = 404;
+      return next(error);
+    }
+    return res.status(200).json(task);
+  })
+  .catch(next);
+
+}
+
+
+module.exports = {
+  index: taskIndex
+};
