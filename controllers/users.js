f402d6c4633ec49854cc3e48146dcefc652b0091
dkhf92 <didrik@forsberg.to>
2017-05-13 13:31:22 +0100

    created users controller

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const User = require('../models/user');
+
+
+function usersIndex(req, res) {
+  User.find((err, users) => {
+    if (err) return res.status(500).json({ message: 'Something went wrong.'});
+    return res.status(200).json(users);
+  });
+}
+
+function usersShow(req, res) {
+  User.findById(req.params.id, (err, user) => {
+    if (err) return res.status(500).json({ message: 'Something went wrong.'});
+    if (!user) return res.statu(404).json({ message: 'User not found.'});
+    return res.status(200).json(user);
+  });
+}
+
+
+
+module.exports = {
+  index: usersIndex,
+  show: usersShow
+
+};
