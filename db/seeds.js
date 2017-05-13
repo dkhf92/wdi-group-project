2b4ecaefb7e0efa28932135cb7c3f31a0983d493
imran787 <imran.mughal87@gmail.com>
2017-05-13 15:59:01 +0100

    added dummy data to seeds.js

--- db/seeds.js
+++ db/seeds.js
@@ -15,6 +15,31 @@ Task
     description: 'Bar shift',
     location: 'London',
     price: 19.99
+  },{
+    name: 'move bed',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    location: 'Harrow',
+    price: 9.99
+  },{
+    name: 'something',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Germany',
+    price: 1.99
+  },{
+    name: 'something else',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Peckham',
+    price: 59.99
+  },{
+    name: 'dog sitting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Croydon',
+    price: 49.99
+  },{
+    name: 'cleaning',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    location: 'Aldgate East',
+    price: 69.99
   }
 ])
 .then(tasks => {
