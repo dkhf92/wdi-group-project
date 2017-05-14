c0a15152e1e6762d15d8f0a8d726b1f8441265c7
HKeating <horacekeating@hotmail.co.uk>
2017-05-14 14:30:00 +0100

    added further tests to POST /api/login, all passing

--- test/user/users_test.js
+++ test/user/users_test.js
@@ -37,7 +37,7 @@ describe('User tests', () => {
     done();
   });
 
-  describe('POST /api/register', () => {
+  describe('User Registration: POST /api/register', () => {
     it('should return a 201 response', done => {
       api
       .post('/api/register')
@@ -67,7 +67,6 @@ describe('User tests', () => {
         })
         .end((err, res) => {
           const response = res.body;
-          console.log('************* Response ***************', response);
 
           expect(response)
             .to.have.property('message')
@@ -78,9 +77,6 @@ describe('User tests', () => {
           expect(response)
             .to.have.property('token')
             .and.to.be.a('string');
-
-
-
 
           done();
         });
@@ -100,7 +96,6 @@ describe('User tests', () => {
         })
         .end((err, res) => {
           const user = res.body.user;
-          console.log('************* USER ***************', user);
 
           expect(user)
             .to.have.property('_id')
@@ -118,11 +113,130 @@ describe('User tests', () => {
             .to.have.property('email')
             .and.to.be.a('string');
 
+          done();
+        });
+    });
 
+  });
+
+
+  describe('User Login: POST /api/login', () => {
+
+    beforeEach(done => {
+      User.collection.remove();
+      api
+      .post('/api/register')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'Test',
+        firstName: 'Horace',
+        lastName: 'Keating',
+        email: 'test@test.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      })
+      .then(() => done())
+        .catch(done);
+    });
+
+    afterEach(done => {
+      User.collection.remove();
+      done();
+    });
+
+    it('should return a 200 response', done => {
+      api
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .expect(200, done);
+    });
+
+    it('should return a message, a user and a token', done => {
+      api
+      .post('/api/login')
+      .set('Accept', 'application/json')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
+      })
+      .end((err, res) => {
+        const response = res.body;
+
+        expect(response)
+          .to.have.property('message')
+          .and.to.be.a('string');
+        expect(response)
+          .to.have.property('user')
+          .and.to.be.a('object');
+        expect(response)
+          .to.have.property('token')
+          .and.to.be.a('string');
+
+        done();
+      });
+    });
+
+    it('should return a user with certain properties', done => {
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'test@test.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          const user = res.body.user;
+
+          expect(user)
+            .to.have.property('_id')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('username')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('firstName')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('lastName')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('email')
+            .and.to.be.a('string');
 
           done();
         });
     });
 
+    it('should return the user that logged in', done => {
+      api
+        .post('/api/login')
+        .set('Accept', 'application/json')
+        .send({
+          email: 'test@test.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          const user = res.body.user;
+
+          expect(user)
+            .to.have.property('username')
+            .and.to.equal('Test');
+          expect(user)
+            .to.have.property('firstName')
+            .and.to.equal('Horace');
+          expect(user)
+            .to.have.property('lastName')
+            .and.to.equal('Keating');
+          expect(user)
+            .to.have.property('email')
+            .and.to.equal('test@test.com');
+
+          done();
+        });
+    });
   });
 });
