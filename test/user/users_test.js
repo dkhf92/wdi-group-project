7ace05ac9b01271eaed0a7040b470b98f63f483b
HKeating <horacekeating@hotmail.co.uk>
2017-05-14 11:37:21 +0100

    continued setting up testing, added test, development and production DB values to config/env

--- test/user/users_test.js
+++ test/user/users_test.js
@@ -2,23 +2,44 @@
 
 require('../spec_helper');
 
-describe('User Controller Test', () => {
-  describe('GET /api/users', () => {
+// describe('User Controller Test', () => {
+//   describe('GET /api/users', () => {
+//     it('should return a 200 response', done => {
+//       api
+//         .get('/api/users')
+//         .set('Accept', 'application/json')
+//         .expect(200, done);
+//     });
+//     it('should return an array of users', done => {
+//       api
+//         .get('/api/users')
+//         .set('Accept', 'application/json')
+//         .end((err, res) => {
+//           expect(res.body).to.be.an('array');
+//           done();
+//         });
+//     });
+//
+//   });
+// });
+
+describe('Registration Test', () => {
+  describe('GET /api/register', () => {
     it('should return a 200 response', done => {
       api
-        .get('/api/users')
+        .get('/api/register')
         .set('Accept', 'application/json')
         .expect(200, done);
     });
-    it('should return an array of users', done => {
-      api
-        .get('/api/users')
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-          expect(res.body).to.be.an('array');
-          done();
-        });
-    });
+    // it('should return an array of users', done => {
+    //   api
+    //     .get('/api/users')
+    //     .set('Accept', 'application/json')
+    //     .end((err, res) => {
+    //       expect(res.body).to.be.an('array');
+    //       done();
+    //     });
+    // });
 
   });
 });
