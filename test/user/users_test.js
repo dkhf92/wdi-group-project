6548aecb12ca59ff69f958125b0a837398bd293a
HKeating <horacekeating@hotmail.co.uk>
2017-05-14 14:05:01 +0100

    3 tests set up for registration process

--- test/user/users_test.js
+++ test/user/users_test.js
@@ -52,15 +52,77 @@ describe('User tests', () => {
       })
       .expect(201, done);
     });
-    // it('should return an array of users', done => {
-    //   api
-    //     .get('/api/users')
-    //     .set('Accept', 'application/json')
-    //     .end((err, res) => {
-    //       expect(res.body).to.be.an('array');
-    //       done();
-    //     });
-    // });
+
+    it('should return a message, a user and a token', done => {
+      api
+        .post('/api/register')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'Test',
+          firstName: 'Horace',
+          lastName: 'Keating',
+          email: 'test@test.com',
+          password: 'password',
+          passwordConfirmation: 'password'
+        })
+        .end((err, res) => {
+          const response = res.body;
+          console.log('************* Response ***************', response);
+
+          expect(response)
+            .to.have.property('message')
+            .and.to.be.a('string');
+          expect(response)
+            .to.have.property('user')
+            .and.to.be.a('object');
+          expect(response)
+            .to.have.property('token')
+            .and.to.be.a('string');
+
+
+
+
+          done();
+        });
+    });
+
+    it('should create a user', done => {
+      api
+        .post('/api/register')
+        .set('Accept', 'application/json')
+        .send({
+          username: 'Test',
+          firstName: 'Horace',
+          lastName: 'Keating',
+          email: 'test@test.com',
+          password: 'password',
+          passwordConfirmation: 'password'
+        })
+        .end((err, res) => {
+          const user = res.body.user;
+          console.log('************* USER ***************', user);
+
+          expect(user)
+            .to.have.property('_id')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('username')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('firstName')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('lastName')
+            .and.to.be.a('string');
+          expect(user)
+            .to.have.property('email')
+            .and.to.be.a('string');
+
+
+
+          done();
+        });
+    });
 
   });
 });
