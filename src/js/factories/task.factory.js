20b05fccf61c64ba58149d6d4a6537f0577ef253
imran787 <imran.mughal87@gmail.com>
2017-05-15 10:19:53 +0100

    created task factory

---
+++ src/js/factories/task.factory.js
@@ -0,0 +1,10 @@
+angular
+  .module('thisApp')
+  .factory('Task', Task);
+
+Task.$inject = ['$resource'];
+function Task($resource){
+  return $resource('http://localhost:4000/api/tasks/:id', { id: '@_id'},{
+    'update': { method: 'PUT'}
+  });
+}
