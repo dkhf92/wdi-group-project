0b193fe8ec9bac728375950d07cf1245986e3d51
HKeating <horacekeating@hotmail.co.uk>
2017-05-15 19:09:47 +0100

    made some more tweaks

--- src/js/controllers/tasks/tasks-show.controller.js
+++ src/js/controllers/tasks/tasks-show.controller.js
@@ -2,29 +2,27 @@ angular
 .module('thisApp')
 .controller('TasksShowCtrl', TasksShowCtrl);
 
-TasksShowCtrl.$inject = ['$stateParams', 'Task', 'CurrentUserService'];
-function TasksShowCtrl($stateParams, Task, CurrentUserService){
+TasksShowCtrl.$inject = ['$stateParams', 'Task', 'CurrentUserService', 'User'];
+function TasksShowCtrl($stateParams, Task, CurrentUserService, User){
   const vm  = this;
 
   vm.user = CurrentUserService.currentUser;
 
-  console.log('Current user: ', vm.user);
   vm.task = Task.get($stateParams);
-console.log('Task: ', vm.task);
+
   vm.request = () => {
     if (vm.task.requestedBy.includes(vm.user._id)) {
       return console.log('Sorry you already requested this job');
     }
-    vm.task.requestedBy.push(vm.user._id); 
+    vm.task.requestedBy.push(vm.user._id);
     Task
       .update({ id: $stateParams.id }, vm.task)
       .$promise
       .then(() => {
         console.log('TASK: ', vm.task);
       });
-
-
   };
 
-
+  vm.requests = User.get(vm.task.requestedBy);
+  console.log(vm.requests);
 }
