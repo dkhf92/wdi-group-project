544ad1ac093850a4693c2a4c050a9dd79babe8cc
HKeating <horacekeating@hotmail.co.uk>
2017-05-15 10:46:36 +0100

    updated charity model after checking justgiving API response

--- models/charity.js
+++ models/charity.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const charitySchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  charityDisplayName: { type: String, required: true },
+  logoUrl: { type: String },
+  charityId: { type: Number },
+  description: { type: String }
 });
 
 module.exports = mongoose.model('Charity', charitySchema);
