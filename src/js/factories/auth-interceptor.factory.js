bc84461ff245f9cfc3835544113be3777b5e1d06
mmorandini <emmemorandini@gmail.com>
2017-05-15 14:21:50 +0100

    working on tokens

--- src/js/factories/auth-interceptor.factory.js
+++ src/js/factories/auth-interceptor.factory.js
@@ -6,12 +6,15 @@ AuthInterceptor.$inject = ['API', 'TokenService'];
 function AuthInterceptor(API, TokenService) {
   return {
     request(config){
-      console.log('req: ', config);
+
+      console.log('config:', config);
+
       return config;
     },
     response(res) {
       if (res.config.url.indexOf(API) === 0 && res.data.token){
         console.log('res.data.token: ', res.data.token);
+        TokenService.setToken(res.data.token);
       }
       console.log('res: ', res);
       return res;
