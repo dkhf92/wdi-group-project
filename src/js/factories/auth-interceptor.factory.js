f9f2ba4945801975286aac7e6f5b25ed572f58ff
HKeating <horacekeating@hotmail.co.uk>
2017-05-15 17:30:59 +0100

    removed some logs

--- src/js/factories/auth-interceptor.factory.js
+++ src/js/factories/auth-interceptor.factory.js
@@ -8,21 +8,16 @@ function AuthInterceptor(API, TokenService) {
     request(config){
       const token = TokenService.getToken();
 
-      console.log('API************', API);
-      console.log('CONFIG.URL************', config.url);
       if (config.url.indexOf(API) === 0 && token){
-        console.log('token is:', token);
         config.headers.Authorization = `Bearer ${token}`;
-        // console.log('config.headers.Authorization: ', config.headers.Authorization);
+
       }
       return config;
     },
     response(res) {
       if (res.config.url.indexOf(API) === 0 && res.data.token){
-        console.log('res.data.token: ', res.data.token);
         TokenService.setToken(res.data.token);
       }
-      // console.log('res: ', res);
       return res;
     }
   };
