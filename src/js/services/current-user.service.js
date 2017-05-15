48bdc4fcb31f2bda972d43c1917b5be3ce6d9c27
mmorandini <emmemorandini@gmail.com>
2017-05-15 16:39:25 +0100

    broadcasting

--- src/js/services/current-user.service.js
+++ src/js/services/current-user.service.js
@@ -8,7 +8,6 @@ function CurrentUserService(TokenService, $rootScope, User) {
 
   self.getUser = () => {
     const decoded = TokenService.decodeToken();
-    console.log('********* THIS IS DECODED ***********', decoded);
 
     if (decoded) {
       User
@@ -16,7 +15,7 @@ function CurrentUserService(TokenService, $rootScope, User) {
         .$promise
         .then(data => {
           self.currentUser = data;
-          console.log('data: ', data);
+          $rootScope.$broadcast('loggedIn');
         });
     }
   };
