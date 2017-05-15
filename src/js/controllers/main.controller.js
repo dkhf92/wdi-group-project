e8e4731457a578cc03b94c82937844c7d3ca0867
dkhf92 <didrik@forsberg.to>
2017-05-15 12:23:07 +0100

    navbar is fucked

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,12 +1,23 @@
 angular
-  .module('thisApp')
-  .controller('MainCtrl', MainCtrl);
+.module('thisApp')
+.controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$rootScope', '$state', '$auth'];
-function MainCtrl($rootScope, $state, $auth) {
+MainCtrl.$inject = ['$rootScope'];
+function MainCtrl($rootScope) {
   const vm = this;
 
-  vm.isNavCollapsed = true;
+  vm.isNavCollapsed  = true;
+  // vm.isAuthenticated = $auth.isAuthenticated;
 
-  $rootScrop
+  // $rootScope.$on('error', (e, err) => {
+  //   vm.stateHasChanged = false;
+  //   vm.message = err.data.message;
+  //   $state.go('login');
+  // });
+
+  $rootScope.$on('$stateChangeSuccess', () => {
+    if(vm.stateHasChanged) vm.message = null;
+    if(!vm.stateHasChanged) vm.stateHasChanged = true;
+    vm.isNavCollapsed = true;
+  });
 }
