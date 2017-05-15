21bb598662be14dc602b43ef148975bf1dd6d91f
dkhf92 <didrik@forsberg.to>
2017-05-15 19:07:34 +0100

    navbar

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -7,7 +7,6 @@ function MainCtrl($rootScope, CurrentUserService, $state) {
   const vm = this;
   $rootScope.$on('loggedIn', () => {
     vm.user = CurrentUserService.currentUser;
-    $state.go('tasksIndex');
   });
   vm.logout = () => {
     CurrentUserService.removeUser();
