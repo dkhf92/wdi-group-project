e41d88abba04d3f55698336d2b73d3e209c6bee5
dkhf92 <didrik@forsberg.to>
2017-05-15 18:40:11 +0100

    :fire

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ function usersIndex(req, res) {
 function usersShow(req, res) {
   User.findById(req.params.id, (err, user) => {
     if (err) return res.status(500).json({ message: 'Something went wrong.'});
-    if (!user) return res.statu(404).json({ message: 'User not found.'});
+    if (!user) return res.status(404).json({ message: 'User not found.'});
     return res.status(200).json(user);
   });
 }
