688a09816aacac0ca6976de6e85f8bf328a6be29
mmorandini <emmemorandini@gmail.com>
2017-05-17 13:38:10 +0100

    hey oh lets go

---
+++ src/js/directives/googleMap.js
@@ -0,0 +1,18 @@
+angular
+  .module('thisApp')
+  .directive('googleMap', googleMap);
+
+googleMap.$inject = ['$window'];
+function googleMap($window){
+  return{
+    restrict: 'E',
+    replace: 'true',
+    template: '<div class="google-map">GOOGLE MAP GOES HERE</div>',
+    link(scope, element){
+      new $window.google.maps.Map(element[0], {
+        zoom: 14,
+        center: { lat: 51.5, lng: -0.07 }
+      });
+    }
+  };
+}
