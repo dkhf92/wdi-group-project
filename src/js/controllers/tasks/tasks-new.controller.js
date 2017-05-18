6825810dae55e62997ff2a87e79ca7effa732367
HKeating <horacekeating@hotmail.co.uk>
2017-05-18 10:22:41 +0100

    set up postcode validation on taskEdit and ng-show condition on edit button

--- src/js/controllers/tasks/tasks-new.controller.js
+++ src/js/controllers/tasks/tasks-new.controller.js
@@ -11,12 +11,14 @@ function TasksNewCtrl($state, Task, CurrentUserService){
   vm.postcodeValidate = /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/;
 
   function taskCreate(){
-    vm.task.createdBy = vm.user._id;
-    Task
-    .save(vm.task)
-    .$promise
-    .then(() => {
-      $state.go('tasksIndex');
-    });
+    if (vm.taskForm.$valid) {
+      vm.task.createdBy = vm.user._id;
+      Task
+      .save(vm.task)
+      .$promise
+      .then(() => {
+        $state.go('tasksIndex');
+      });
+    }
   }
 }
